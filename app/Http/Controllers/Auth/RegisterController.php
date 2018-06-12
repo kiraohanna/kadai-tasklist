4629fea2b96f51620267e60091c1b278444d3555
kanna.hirao <kanna.hirao@rakuten.com>
2018-06-12 05:09:31 +0000

    kokomade ok

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -36,7 +36,7 @@ class RegisterController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('guest')->excerpt('logout');
+        $this->middleware('guest')->except('logout');
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,3 @@ Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('login', 'Auth\LoginController@login')->name('login.post');
 Route::get('logout', 'Auth\LoginController@logout')->name('logout.get');
 
-Route::get('/', 'TasksController@index');
-
-Route::resource('tasks', 'TasksController');
-
